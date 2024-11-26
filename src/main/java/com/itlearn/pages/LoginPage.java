552cd282003e992e52380890d32630bccea822d9
Chandraketu Sharma <sharma123ronit@gmail.com>
2024-11-26 22:11:25 +0530

    FirstCommit

---
+++ src/main/java/com/itlearn/pages/LoginPage.java
@@ -0,0 +1,49 @@
+package com.itlearn.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import javax.xml.xpath.XPath;
+
+public class LoginPage {
+    WebDriver driver;
+    //constructor
+    public LoginPage(WebDriver driver1)
+    {
+        this.driver = driver1;
+        PageFactory.initElements(driver,this);
+    }
+    @FindBy(xpath="//a[contains(text(),'Log in')]")
+    WebElement loginLink;
+
+    @FindBy(xpath = "//input[@name=\"log\"]")
+    WebElement usernameField;
+
+    @FindBy(xpath = "//input[@name='pwd']")
+    WebElement passwordField;
+
+    @FindBy(xpath = "//input[@type=\"submit\"]")
+    WebElement loginButton;
+
+    @FindBy(xpath = "//i[@class='fa fa-user']")
+    WebElement logoutimage;
+
+    @FindBy(xpath = "//a[contains(text(),'Logout')]")
+    WebElement logoutclick;
+
+
+    public void loginPortal(String username,String password)
+    {
+        loginLink.click();
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        loginButton.click();
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/itlearn/testcases/LoginTestDataDrivenTesting.java
@@ -0,0 +1,42 @@
+package com.itlearn.testcases;
+
+import com.itlearn.pages.BaseTest;
+import com.itlearn.pages.LoginPage;
+import com.itlearn.utility.ReadExcel;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class LoginTestDataDrivenTesting extends BaseTest { // Assuming BaseTest initializes the WebDriver
+
+    String fileName = System.getProperty("user.dir") + "\\testData\\Book1.xlsx";
+
+    @Test(priority = 1, dataProvider = "LoginDataProvider")
+    public void verifyLogin(String userEmail, String userPwd) {
+        // Assuming LoginPage is a valid class
+        LoginPage lp = new LoginPage(driver); // Ensure 'driver' is initialized in BaseTest
+
+        // Use the provided userEmail and userPwd to log in
+        lp.loginPortal(userEmail, userPwd);
+    }
+
+    @DataProvider(name = "LoginDataProvider")
+    public Object[][] LoginDataProvider() throws IOException {
+        // Read row and column counts from the Excel file
+        int ttlRows = ReadExcel.getRowCount(fileName, "Sheet1");
+        int ttlColumns = ReadExcel.getColCount(fileName, "Sheet1");
+
+        // Initialize the data array
+        String[][] data = new String[ttlRows - 1][ttlColumns];
+
+        // Fill the data array with values from the Excel file
+        for (int i = 1; i < ttlRows; i++) {
+            for (int j = 0; j < ttlColumns; j++) {
+                data[i - 1][j] = ReadExcel.getCellValue(fileName, "Sheet1", i, j);
+            }
+        }
+
+        return data;
+    }
+}
